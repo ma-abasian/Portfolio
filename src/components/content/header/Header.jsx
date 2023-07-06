d7406940e53ad6fdae4cc4a056e7714ee7447ef1
ma-abasian <abasianmohammadali@gmail.com>
2023-07-06 16:53:34 +0330

    fix bugs

--- src/components/content/header/Header.jsx
+++ src/components/content/header/Header.jsx
@@ -1,12 +1,12 @@
 import Image from "./Image.jsx";
-//import HeadImage from '../../../assets/images/home-banner.png';
+import HeadImage from '../../../assets/images/home-banner.png';
 import  '../../../assets/css/content/header/header.css';
 import HeaderContent from "./HeaderContent.jsx";
 
 const Header = () => {
     return (
         <div className="home__header">
-            <Image imageSrc="https://portfolio-dun-chi-37.vercel.app/home-banner.png"/>
+            <Image imageSrc={HeadImage}/>
             igiigiugi21212
             <HeaderContent/>
         </div>
