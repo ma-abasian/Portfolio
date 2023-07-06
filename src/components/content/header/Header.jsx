e47efa6929210b2cc1f2131104339b4d461a75e2
ma-abasian <89036985+ma-abasian@users.noreply.github.com>
2023-07-06 16:26:29 +0330

    Update Header.jsx

--- src/components/content/header/Header.jsx
+++ src/components/content/header/Header.jsx
@@ -1,12 +1,12 @@
 import Image from "./Image.jsx";
-import HeadImage from '../../../assets/images/home-banner.png';
+//import HeadImage from '../../../assets/images/home-banner.png';
 import  '../../../assets/css/content/header/header.css';
 import HeaderContent from "./HeaderContent.jsx";
 
 const Header = () => {
     return (
         <div className="home__header">
-            <Image imageSrc={HeadImage}/>
+            <Image imageSrc="/src/assets/images/home-banner.png"/>
             <HeaderContent/>
         </div>
     );
