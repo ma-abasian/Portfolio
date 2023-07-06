b8892a439c06bb1b1fc5a16cf9c4b03ab0e28e66
ma-abasian <89036985+ma-abasian@users.noreply.github.com>
2023-07-06 16:38:02 +0330

    Update Header.jsx

--- src/components/content/header/Header.jsx
+++ src/components/content/header/Header.jsx
@@ -6,7 +6,8 @@ import HeaderContent from "./HeaderContent.jsx";
 const Header = () => {
     return (
         <div className="home__header">
-            <Image imageSrc="https://portfolio-dun-chi-37.vercel.app/src/assets/images/home-banner.png"/>
+            <Image imageSrc="https://portfolio-dun-chi-37.vercel.app/assets/images/home-banner.png"/>
+            igiigiugi
             <HeaderContent/>
         </div>
     );
